dc0242c670153a53bd8f4c6308046e57b015413c
Artem Strohin <artem.strohin@gmail.com>
2025-06-05 22:51:34 +0100

    feat: add frontend

---
+++ backend/src/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,  # type: ignore
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return "alive"
+
